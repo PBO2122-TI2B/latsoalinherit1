93f8b2718916b949d83fe5b01ba259a21310b6ba
Akhmad Ramadani <ar20011118@gmail.com>
2021-10-12 09:12:40 +0700

    05-Akhmad Ramadani

---
+++ BangunDatar.java
@@ -0,0 +1,13 @@
+public class BangunDatar {
+
+    public float luas() {
+        float result = 0f;
+        return result;
+    }
+
+    public float keliling() {
+        float result = 0f;
+        return result;
+    }
+
+}
