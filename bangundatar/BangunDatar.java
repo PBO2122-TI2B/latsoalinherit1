fcc260db88dfc7fb7f7aeafbdc1bafb0aa0af61a
RadityaCatur <91645527+RadityaCatur@users.noreply.github.com>
2021-11-02 13:21:13 +0700

    22_Raditya Catur Narendra (Baru)

---
+++ bangundatar/BangunDatar.java
@@ -0,0 +1,11 @@
+package bangundatar;
+
+public class BangunDatar {
+    public float hitungLuas(float luas) {
+        return luas;
+    }
+
+    public float hitungKeliling(float keliling) {
+        return keliling;
+    }
+}
