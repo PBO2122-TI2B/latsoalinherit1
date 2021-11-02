9a2f652cbd079cc851a835a16ae6c911482e8cec
AlifiyulAkyun <73621480+AlifiyulAkyun@users.noreply.github.com>
2021-11-02 13:48:24 +0700

    Add files via upload

---
+++ BangunDatar.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo6;
+
+/**
+ *
+ * @author ALIFIYUL
+ */
+
+public class BangunDatar {
+    
+    float luas(){
+        System.out.println("Menghitung laus bangun datar");
+        return 0;
+    }
+    
+    float keliling(){
+        System.out.println("Menghitung keliling bangun datar");
+        return 0;
+    }
+    
+}
