0049fa32d1ee37b2f9a2553a4709d7d0b6f91413
Dka-11 <91647072+Dka-11@users.noreply.github.com>
2021-11-03 20:30:05 +0700

    Add files via upload

--- Segitiga.java
+++ Segitiga.java
@@ -3,19 +3,17 @@ public class Segitiga extends BangunDatar{
     public float tinggi;
     public float c;
 
+    @Override
     public float luas(){
         float luas = (float) 0.5 * (alas * tinggi);
         System.out.println("Luas Segitiga : " + luas);
         return luas;
     }
 
-    public float NilaiC(){
-        float c = (float) Math.sqrt(Math.pow(alas, 2) + Math.pow(tinggi, 2));
-        return c;
-    }
-
+    @Override
     public float keliling(){
-        float keliling = alas + tinggi + NilaiC();
+        float c = (float) Math.sqrt(Math.pow(alas, 2) + Math.pow(tinggi, 2));
+        float keliling = alas + tinggi + c;
         System.out.println("Keliling Segitiga : " +keliling);
         return keliling;
     }
